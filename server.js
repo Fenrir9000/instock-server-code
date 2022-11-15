7493047e65abf51056d2366a2fa9d8a7b43381e6
Miloan <miloan27@hotmail.com>
2022-11-14 21:41:58 -0500

    initial repo

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+
+const { PORT } = process.env;
+
+const inventoryRoutes = require("./routes/inventories");
+const warehouseRoutes = require("./routes/warehouse");
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/inventory", inventoryRoutes);
+app.use("/warehouses", warehouseRoutes);
+
+app.listen(PORT, function () {
+  console.log(`CORS-enabled web server listening on port ${PORT}`);
+});
